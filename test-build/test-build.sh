44d83e67d336a280ff866cfb297f49c9c11819cb
XiaoluHong <xiaoluhong@rancher.com>
2018-03-02 16:02:10 +0800

    no message

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 
+#自定义变量
 RANCHER_URL=${RANCHER_URL}
 ACCESS_KEY=${ACCESS_KEY}
 SECRET_KEY=${SECRET_KEY}
 ENV=${ENV:-Default}
 STACKS=${STACKS}
+REGISTRY=${REGISTRY}
+PROJECT=${PROJECT}
+LB_TAG=${LB_TAG}
+
+
+cd ${JOB_NAME}
+
+# Get new tags from remote  
+git fetch --tags
+# Get latest tag name  
+BUILD_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)  
 
 cd ${WORKSPACE}/src
 
-docker build -t 10.211.55.16:80/python-redis-demo:${BUILD_NUMBER} .
-
-
-docker push 10.211.55.16:80/python-redis-demo:${BUILD_NUMBER}
+docker build -t ${REGISTRY}/${PROJECT}/${JOB_NAME}:${BUILD_TAG} .
+docker push ${REGISTRY}/${PROJECT}/${JOB_NAME}:${BUILD_TAG} 
 
 cd ${WORKSPACE}/test-build
 
+sed -i 's/\$\$REGISTRY\$\$/'${REGISTRY}'/g' docker-compose.yml
+sed -i 's/\$\$PROJECT\$\$/'${PROJECT}'/g' docker-compose.yml
+sed -i 's/\$\$JOB_NAME\$\$/'${JOB_NAME}'/g' docker-compose.yml
 sed -i 's/\$\$BUILD_TAG\$\$/'${BUILD_TAG}'/g' docker-compose.yml
-
-sed -i 's/\$\$BUILD_TAG\$\$/'`expr 5000 + ${BUILD_TAG}`'/g' docker-compose.yml
+sed -i 's/\$\$LB_TAG\$\$/'${LB_TAG}'/g' docker-compose.yml
 
 chmod 777 ./rancher
 
